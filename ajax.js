cfcbb5c5ce5fe769c070ce5d15774c5b5a4bf479
wanghc00 <306093226@qq.com>
2019-07-14 15:00:49 +0800

    作业

---
+++ ajax.js
@@ -0,0 +1,19 @@
+let ajax = {
+            request(url, method, callback, params) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = (function (myxhr) {
+                    return function () {
+                        if (myxhr.readyState === 4 && myxhr.status === 200) {
+                            callbak(myxhr);
+                        }
+                    }
+                })(xhr);
+                xhr.open(method, url, true);
+                xhr.send(params || '');
+            }
+        }
+function myCallback(xhr) {
+	alert(xhr.responseText);
+}
+ajax.request('somefile.txt', 'get', myCallback);
+ajax.request('script.php', 'post', myCallback, 'first=John&last=Smith');
